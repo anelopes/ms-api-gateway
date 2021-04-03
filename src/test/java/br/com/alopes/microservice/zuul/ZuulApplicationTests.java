e686b340602cbe74482838fdf9040e74bfd029e2
anelopes <anelopes17@outlook.com>
2021-04-02 22:46:53 -0300

    Microservice - Project Zuul

---
+++ src/test/java/br/com/alopes/microservice/zuul/ZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alopes.microservice.zuul;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
